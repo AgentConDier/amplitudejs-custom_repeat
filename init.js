75b5349366b3dbf63dff222d6de85cd128e4aad8
AgentConDier <Darnok22221@gmx.de>
2018-01-20 20:35:31 +0100

    Create init.js

---
+++ init.js
@@ -0,0 +1,15 @@
+Amplitude.init({'songs':[{
+    "url":"http://darnok2221.no-ip.biz/archive/dYpGXp0t8ZfctS834dn1.mp3",
+  },{
+    "url":"http://darnok2221.no-ip.biz/archive/SVq0BAye02Jo9XptkRoA.mp3",
+  }
+],'callbacks':{
+  'song_change': function(){
+    if (repeat) Amplitude.playNow(repeatedSong);
+  }
+}});
+document.getElementById('song-played-progress').addEventListener('click', function( e ){
+  var offset = this.getBoundingClientRect();
+  var x = e.pageX - offset.left;
+  Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
+});
